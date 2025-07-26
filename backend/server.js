e4f7016a6d4d29f62423e0a0500f45b7af63d483
harpith <harpithrao2004@gmail.com>
2025-07-26 17:05:46 +0530

    Start

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express=require("express");
+const app=express();
+const { chats } =require("./data/data");
+const dotenv=require("dotenv");
+dotenv.config();
+
+
+app.get("/",(req,res)=>{
+    res.send("API is running");
+});
+
+app.get("/api/chat",(req,res)=>{
+    res.send(chats);
+});
+
+app.get("/api/chat/:id",(req,res)=>{
+    const { id }=req.params;
+    const chat=chats.find((c)=>c._id==id);
+    res.send(chat);    
+});
+
+
+
+
+const PORT=process.env.PORT || 5000;
+
+app.listen(5000,()=>{
+    console.log("Server is running on port 5000")
+});
+
+
